1406f1cc7bd66a270a0f3e34a31f451c36e97cc0
altunoymak <yusuf_altunoymak@hotmail.com>
2024-05-28 06:13:45 +0300

    Menu Created and Home Fragment UI

--- app/src/main/java/com/altunoymak/eterationemarketcasestudy/MainActivity.kt
+++ app/src/main/java/com/altunoymak/eterationemarketcasestudy/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.altunoymak.eterationemarketcasestudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment.navController)
 
     }
 }
---
+++ app/src/main/java/com/altunoymak/eterationemarketcasestudy/presentation/ui/cart/BasketFragment.kt
@@ -0,0 +1,9 @@
+package com.altunoymak.eterationemarketcasestudy.presentation.ui.cart
+
+import com.altunoymak.eterationemarketcasestudy.base.BaseFragment
+import com.altunoymak.eterationemarketcasestudy.databinding.FragmentBasketBinding
+
+
+class BasketFragment : BaseFragment<FragmentBasketBinding>(FragmentBasketBinding::inflate) {
+
+}
